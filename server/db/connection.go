ba705e760aa8de1ec0eb1ca602639beff00cd8ce
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-03-28 16:25:28 -0700

    Update connections.go

--- server/db/connection.go
+++ server/db/connection.go
@@ -22,19 +22,7 @@ func init() {
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("No env file found in the directory")
 	}
-
-	// uri := os.Getenv("MONGODB_URI")
-
-	// if uri == "" {
-	// 	log.Fatal("Set the MONGODB_URI in the env")
-	// }
-
-	// client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
+	
 	ctx,cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
