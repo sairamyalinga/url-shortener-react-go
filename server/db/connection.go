ccdab649d3d6691c1a04de66502ac3fc03f26f4d
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-04-02 01:02:01 -0700

    insert document in mongoDB function

--- server/db/connection.go
+++ server/db/connection.go
@@ -3,38 +3,47 @@ package connection
 import (
 	"context"
 	"fmt"
-	"time"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
-	_"go.mongodb.org/mongo-driver/bson"
+	_ "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-type urlStrings struct{
-	Url string `bson:"url, omitempty"`
-	Id  primitive.ObjectID  `json:_id bson:"_id`
+
+type URLStrings struct {
+	Url string             `bson:"url, omitempty"`
+	Id  primitive.ObjectID `json:_id bson:"_id`
 }
+
 var client *mongo.Client
+
 func init() {
 
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("No env file found in the directory")
 	}
-	
-	ctx,cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+
+	// client = mclient
 	if err != nil {
 		fmt.Println("Error connecting to MongoDB:", err)
-		return 
+		return
 	}
+
+	fmt.Printf("Connection succes")
 
 	defer client.Disconnect(ctx)
 
 }
 
 func GetClient() *mongo.Client {
-    return client
+	return client
 }
--- server/routes/router.go
+++ server/routes/router.go
@@ -1,5 +1,63 @@
 package router
 
-func Router(){
+import (
+	"context"
+	"fmt"
+	"net/http"
+	connection "urlShortener/server/db"
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/shorturl", CreateUrl).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/shorturl/{id}", RedirectUrl).Methods("GET", "OPTIONS")
 	
+
+	// router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//     http.ServeFile(w, r, "urlShortener/frontend/index.html")
+	// })
+
+	return router
 }
+
+func CreateUrl(w http.ResponseWriter, r *http.Request) {
+	// params := mux.Vars(r)
+
+	var urlData map[string]string
+	if err := json.NewDecoder(r.Body).Decode(&urlData); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	client := connection.GetClient()
+	collection := client.Database("go").Collection("urlStrings")
+	
+	isValidURL(urlData)
+	insertURL(r.Context(), collection, urlData)
+
+}
+func isValidURL(urlObj map[string]string){
+
+}
+func insertURL(ctx context.Context, collection *mongo.Collection, object  map[string]string) {
+	
+	newID := primitive.NewObjectID()
+	doc := connection.URLStrings{Url:object["url"], Id: newID}
+
+	_, err := collection.InsertOne(context.TODO(), doc)
+	if err!= nil{
+		fmt.Println("Failed to insert the new document")
+
+	}
+	fmt.Println("Inserted New document")
+
+}
+func RedirectUrl(w http.ResponseWriter, r *http.Request) {
+
+}
+
