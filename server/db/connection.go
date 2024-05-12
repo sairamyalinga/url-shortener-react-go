01221fa0a572ab301d13292f6f729ba8884b4d34
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-05-12 14:12:50 -0700

    ping mongoDB

--- server/db/connection.go
+++ server/db/connection.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	_ "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -28,9 +29,11 @@ func init() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 
+	// fmt.Println(os.Getenv("MONGODB_URI"))
 	var err error
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")).SetServerAPIOptions(serverAPI))
 
 	// client = mclient
 	if err != nil {
@@ -38,9 +41,12 @@ func init() {
 		return
 	}
 
-	fmt.Printf("Connection succes")
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	  }
+	  fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 
-	defer client.Disconnect(ctx)
+	//defer client.Disconnect(ctx)
 
 }
 
