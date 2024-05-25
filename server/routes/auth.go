b6b8e9f3f58dc13e39aee23eb8ae8a018ef6d817
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-05-24 18:46:34 -0700

    Check existing username

--- server/routes/auth.go
+++ server/routes/auth.go
@@ -9,10 +9,10 @@ import (
 	connection "urlShortener/server/db"
 
 	"golang.org/x/crypto/bcrypt"
-	_"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
 	_"go.mongodb.org/mongo-driver/bson/primitive"
 	"github.com/go-playground/validator/v10"
-	_"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 
@@ -43,6 +43,20 @@ func RegisterUser(w http.ResponseWriter, r *http.Request){
 	user.Password = string(bytes)
 	client := connection.GetClient()
 	collection := client.Database("go").Collection("user")
+
+	var existuser connection.User
+	err = collection.FindOne(context.TODO(), bson.M{"username": user.UserName}).Decode(&existuser)
+
+	if err == nil {
+		// fmt.Println("UserName Already Exists!")
+		http.Error(w, "Username already exists", http.StatusConflict)
+		return
+
+	} else if err != mongo.ErrNoDocuments {
+		// An unexpected error occurred
+		http.Error(w, "Error checking username availability", http.StatusInternalServerError)
+		return
+	}
 
 	_, err = collection.InsertOne(context.TODO(), user)
 	if err!= nil{
