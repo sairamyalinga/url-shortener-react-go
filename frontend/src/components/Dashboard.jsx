f2136ffd0d518ab92898e0ec8f9bc8860cfc422b
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-05-28 20:20:54 -0700

    Adding Signin Component

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import Header from './Header.jsx'
+import axios from 'axios'
+
+function Dashboard(){
+    const [shortURL, setShortURL] = useState('');
+    const [showURL, setShowURL] = useState(false);
+  
+    const handleClick = () =>{
+      const requestData = document.getElementById('urlinput').value;
+      console.log(requestData)
+      axios.post('http://localhost:5050/api/shorturl',{url:requestData})
+        .then(response =>{
+          console.log(response.data)
+          setShortURL(response.data.shortURL)
+          setShowURL(true)
+        })
+        .catch(error =>{
+          console.log('Error', error)
+        });
+    };
+  
+    const handleCopyToClipboard = () =>{
+      navigator.clipboard.writeText(shortURL)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        alert('ShortURL copied to clipboard!');
+      })
+      .catch(err => {
+        console.error('Failed to copy text: ', err);
+      });
+    };
+  
+    const handleClose = () =>{
+      setShowURL(false)
+      setShortURL('')
+    }
+
+    return (
+        <div>
+        <Header/>
+        <div className="container" style={{ marginTop: '200px' }}>
+        <div className="row">
+          <div className="col-md-8">
+            <div className="input-group">
+              <input type="url" id = "urlinput" className="form-control rounded-pill-8" placeholder="https://" />
+              <button type="button" className="btn btn-info" onClick={handleClick} >Shorten!</button>
+            </div>
+            <div>
+            {showURL && (
+        <div className="short-url-container">
+          <p>Short URL: {shortURL}</p>
+          <button className="btn btn-secondary" onClick={handleCopyToClipboard}>Copy to Clipboard</button>
+          <button className="btn btn-danger" onClick={handleClose}>Close</button>
+        </div>
+      )}
+          </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  
+}
+
+export default Dashboard;
---
+++ frontend/src/components/Signin.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+
+function Signin() {
+    const [changeForm, setChangeForm] = useState(false);
+    
+    return (
+        <div className="bg-container">
+            <div className="form-box">
+                <div className="toggle-area">
+                    <button 
+                        className={`toggle-button ${changeForm ? "" : "active"}`} 
+                        onClick={() => setChangeForm(false)}
+                    >
+                        Login
+                    </button>
+                    <button 
+                        className={`toggle-button ${changeForm ? "active" : ""}`} 
+                        onClick={() => setChangeForm(true)}
+                    >
+                        SignUp
+                    </button>
+                </div>
+
+                <form className={`input-group ${changeForm ? "hidden" : ""}`}>
+                    <input type="text" className="login-input" autoComplete="off" placeholder="Enter User Name"/>
+                    <input type="password" className="login-input" placeholder="Enter Password"/>
+                    <button>Login</button>
+                </form>
+
+                <form className={`input-group ${changeForm ? "" : "hidden"}`}>
+                    <input type="text" className="login-input" autoComplete="off" placeholder="Enter User Name"/>
+                    <input type="password" className="login-input" placeholder="Enter Password"/>
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Signin;
