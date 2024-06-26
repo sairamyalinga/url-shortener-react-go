367f9f5122e52ff047a141227c87df267aa204c8
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-06-26 12:27:42 -0700

    refactor main

--- server/main.go
+++ server/main.go
@@ -1,30 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 	router "urlShortener/server/routes"
-	"github.com/gorilla/handlers"
 
-	
+	"github.com/gorilla/handlers"
 )
 
 func main() {
-
 	r := router.Router()
-	
-	fmt.Println("Starting the server on port 5050")
-	err := http.ListenAndServe(":5050", 
-				handlers.CORS(
-					handlers.AllowedOrigins([]string{"*"}),
-					handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
-					handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
-				)(r))
+    port := ":"+ os.Getenv("PORT")
+	frontend := os.Getenv("FRONTEND")
+	err := http.ListenAndServe(port, 
+		handlers.CORS(
+			handlers.AllowedOrigins([]string{frontend}), 
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+			handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+		)(r))
 	if err != nil {
 		log.Fatalln("Error with the server,", err)
 	}
-
-
-
 }
+
