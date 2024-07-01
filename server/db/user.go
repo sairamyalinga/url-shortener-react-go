44c1d3150c1b72468cef8d7173fd26d9422dea0d
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-07-01 16:26:51 -0700

    Refactor user db functions

--- server/db/user.go
+++ server/db/user.go
@@ -1,9 +1,40 @@
 package connection
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type User struct {
 	UserID   primitive.ObjectID `bson:"_id,omitempty"`
 	Username string             `json:"user_name" validate:"required,min=4,max=30"`
 	Password string             `json:"password" validate:"required,min=8,max=30"`
+}
+
+var UserExistErr string = "username already exists"
+func (db *DBConnection) InsertUser(ctx context.Context, user User) (error) {
+	
+	var existingUser User 
+	err := db.userCollection.FindOne(ctx, bson.M{"username": user.Username}).Decode(&existingUser)
+
+	if err == nil {	
+		return fmt.Errorf(UserExistErr)
+
+	} else if err != mongo.ErrNoDocuments {
+		// http.Error(w, "Error checking username availability", http.StatusInternalServerError) //bad response format
+		return fmt.Errorf("failed to signup. Try again : %v", err)
+	}
+	_, err = db.userCollection.InsertOne(ctx, user)
+	if err != nil {
+		return fmt.Errorf("failed to insert the new document: %v", err)
+	}	
+	return nil
+}
+
+func (db *DBConnection) FindUserbyName(ctx context.Context, username User) (error) {
+	
 }
