90277a6a0cd12a3fca1f29384e009fe87a4474e9
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-07-02 17:47:22 -0700

    generate refresh token along with access token to handle token expiry

--- server/router/auth.go
+++ server/router/auth.go
@@ -9,18 +9,32 @@ import (
 
 var secretKey = []byte(os.Getenv("JWT_KEY"))
 
-func generateJWT(username string) (string, error) {
+func generateJWT(username string) (map[string]string, error) {
 	claims := jwt.MapClaims{
 		"sub": username,
-		"exp": time.Now().Add(24 * time.Hour).Unix(),
+		"exp": time.Now().Add(time.Minute *2 ).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err := token.SignedString(secretKey)
 	if err != nil {
-		return "", err
+		return nil, err
+	}
+    rtClaims := jwt.MapClaims{
+		"sub" : username,
+		"exp": time.Now().Add(time.Hour * 48).Unix(),
+	}
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+
+	rtoken, err := refreshToken.SignedString(secretKey)
+	if err != nil {
+		return nil, err
 	}
 
-	return tokenString, nil
+	return map[string]string {
+		"access_token":tokenString,
+		"refresh_token": rtoken,
+	}, nil
+
 }
 
 
